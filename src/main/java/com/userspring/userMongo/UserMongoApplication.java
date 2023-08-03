17049a7443d017bb5f8960019b2640cb4a6e935f
wmvieira-philips <william.vieira@philips.com>
2023-08-03 12:39:11 -0300

    start project

---
+++ src/main/java/com/userspring/userMongo/UserMongoApplication.java
@@ -0,0 +1,13 @@
+package com.userspring.userMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/userspring/userMongo/UserMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userspring.userMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
